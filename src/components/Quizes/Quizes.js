24dc648c68b5fdd4cff747e118cd068e150b1cd3
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-10-11 22:13:38 +0600

    statistics chart added

--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import QuizDetails from "../QuizDetails/QuizDetails";
+import "./Quizes.css";
 
 const Quizes = () => {
   const datas = useLoaderData();
   console.log(datas);
   const quizes = datas.data.questions;
   return (
-    <div>
-      <h1>Quiz of {datas.data.name}</h1>
+    <div className="quiz-page">
+      <h1 className="fw-bold text-white">Quiz of {datas.data.name}</h1>
       {quizes.map((quiz, idx) => (
         <QuizDetails idx={idx} key={quiz.id} quiz={quiz}></QuizDetails>
       ))}
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import "./Statistics.css";
 
 const Statistics = () => {
+  const datas = useLoaderData();
+  const data = datas.data;
+  console.log(data);
+
   return (
-    <div>
-      <h1>This is Statistics</h1>
+    <div className="statistics mt-5 mx-auto">
+      <BarChart
+        width={400}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <Bar dataKey="total" stackId="a" fill="#82ca9d" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip></Tooltip>
+      </BarChart>
     </div>
   );
 };
